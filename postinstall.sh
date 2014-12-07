418c03021d518f3e7cd3346db1b5a693dc1afea6
Alek Ratzloff <alekratz@gmail.com>
2014-12-06 20:00:46 -0500

    Simplified some of the host stuff, hopefully this will fix the infinite /etc/hosts bug

--- postinstall.sh
+++ postinstall.sh
@@ -27,14 +27,14 @@ NAMENODE=192.168.1.1
 
 # Hosts that will go into the hosts file
 HOSTS=(
-  "192.168.1.1    192.168.1.1  hydrogen"
-  "192.168.1.2    192.168.1.2  helium"
-  "192.168.1.3    192.168.1.3  lithium"
-  "192.168.1.4    192.168.1.4  beryllium"
-  "192.168.1.5    192.168.1.5  boron"
-  "192.168.1.6    192.168.1.6  carbon"
-  "192.168.1.7    192.168.1.7  nitrogen"
-  "192.168.1.8    192.168.1.8  oxygen"
+  "192.168.1.1 hydrogen"
+  "192.168.1.2 helium"
+  "192.168.1.3 lithium"
+  "192.168.1.4 beryllium"
+  "192.168.1.5 boron"
+  "192.168.1.6 carbon"
+  "192.168.1.7 nitrogen"
+  "192.168.1.8 oxygen"
 )
 
 function print_fail {
